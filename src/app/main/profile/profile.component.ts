f155b3a907cb03121c9e25bd79436e08d020c437
Juanjo <110014245+juanjo-alvarez@users.noreply.github.com>
2023-07-05 14:42:21 +0200

    Password validator

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AuthService, OFormComponent } from 'ontimize-web-ngx';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AuthService, OFormComponent, OPasswordInputComponent } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-profile',
@@ -9,13 +10,41 @@ import { AuthService, OFormComponent } from 'ontimize-web-ngx';
 export class ProfileComponent implements OnInit {
 
   @ViewChild('form',{static:true}) form:OFormComponent;
+  @ViewChild('pwdconf',{static:true}) pwdconf:OPasswordInputComponent;
 
-  constructor(private auth:AuthService) { }
+  public validatorArray: ValidatorFn[] = [];
+
+  constructor(private auth:AuthService) {
+  }
 
   ngOnInit() {
+    this.validatorArray.push(this.matchValidator);
   }
 
   ngAfterViewInit(){
     this.form.queryData({USER_:this.auth.getSessionInfo().user});
+  }
+
+  formInit(){
+    this.form.setFieldValue("PASSWORDCONFIRM",this.form.getFieldValue("PASSWORD"));
+  }
+
+  public matchValidator(control: FormControl): ValidationErrors {
+    try {
+      const password = control.parent ? control.parent.controls['PASSWORD'].value : null
+      const passwordConfirm = control.parent ? control.parent.controls['PASSWORDCONFIRM'].value : null
+      if(password && passwordConfirm && password != passwordConfirm){
+        return { mustMatch: true }
+      }else{
+        return null;
+      }
+    }catch(e){
+      return null;
+    }
+  }
+
+  public reviewMatches(event: Event){
+    this.form.formGroup.controls['PASSWORDCONFIRM'].updateValueAndValidity();
+    this.form.formGroup.get('PASSWORDCONFIRM').markAsTouched();
   }
 }
